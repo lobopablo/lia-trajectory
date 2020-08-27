e1008242f7d205ef83617b7d9c6ac2bdf5a5374e
eng-lobo <66334874+eng-lobo@users.noreply.github.com>
2020-08-26 23:15:29 -0300

    Add files via upload

--- documentation/c.py
+++ documentation/c.py
@@ -10,24 +10,34 @@
 #%% Packages
 import numpy as np
 #
-#%% Atmospheric Properties
-#
-atm_temp0 = 288.15      # [K] - Temperature @ SL
-atm_p0 = 101325         # [Pa] - Pressure @ SL
-atm_rho0 = 1.225        # [kg/m^3] - Density @ SL
-atm_l = 0.0065          # [K/m] - Temperature lapse rate
-atm_r = 8.31447         # [J/(mol*K)] - Ideal Gas Constants
-atm_m = 0.0289654       # [kg/mol] - Dry Air Molar Mass
-atm_g = 9.81            # [m/s^2] - Gravity @ SL
-#
 #%% Launch Vehicle Properties
+# Mass
+M_st1_i = 5826                       # [kg] - Initial mass, stage #1
+M_st1_f = 2026                       # [kg] - Final mass, stage #1 
+M_st1_prop = M_st1_i - M_st1_f       # [kg] - Propellant mass, stage #1
+M_st2_i = 1560                       # [kg] - Initial mass, stage #2
+M_st2_f = 210                        # [kg] - Final mass, stage #2
+M_st2_prop = M_st2_i - M_st2_f       # [kg] - Propellant mass, stage #1
+M_payload = 75                       # [kg] - Payload mass
 #
-me = 43                 # [kg] - Initial Mass
-mprop = 22              # [kg] - Propellant Mass
-cd = 0.35               # [adim] - Drag Coefficient
-aref= np.pi*(0.1**2)    # [m^2] - Reference Area
-dt_burn = 10            # [s] - Impulse Time
-thrust0 = 5500          # [N] - Initial Thrust @ SL
-Ae = 0.0059             # [m^2] - Exhaust area               
-Pe = 101325             # [Pa] - Exhaust Pressure
-L = 1                   # [m] - Lenght of the vehicle
+# Engine performance
+Pe_st1 = 200 * 10**5                 # [N/m^2] - Exhaust presure, stage #1
+Pe_st2 = 200 * 10**5                 # [N/m^2] - Exhaust pressure, stage #2
+ISP_st1_SL = 272                     # [s] - ISP at SL, stage #1
+ISP_st1_V = 293                      # [s] - ISP at Vacuum, stage #1
+ISP_st2_V = 328                      # [s] - ISP at Vacuum, stage #2
+#
+# Propellant consumption
+bt_st1 = 106                         # [s] - Burning time, stage #1
+bt_st2 = 113                         # [s] - Burning time, stage #2
+m_dot_st1 = M_st1_prop/bt_st1        # [kg/s] - Mass flow, stage #1
+m_dot_st2 = M_st2_prop/bt_st2        # [kg/s] - Mass flow, stage #2
+# 
+# 
+
+
+#%% Physical constants
+g = 9.81                       # [m/s^2] - Gravity's acceleration
+
+
+
